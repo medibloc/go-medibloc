8ad5d8783df07b1cc6b74def331afd6b5d1e3020
Cha Minkyoo <minkyoo.cha@medibloc.org>
2018-03-23 16:59:34 +0900

    Add test for config

---
+++ medlet/config_test.go
@@ -0,0 +1,38 @@
+package medlet
+
+import (
+	"flag"
+	"github.com/gogo/protobuf/proto"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+var update = flag.Bool("update", false, "update .golden files")
+
+func init() {
+	flag.Parse()
+}
+
+func TestConfigNotExist(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "config")
+	require.Nil(t, err)
+	os.Remove(f.Name())
+
+	pb := LoadConfig(f.Name())
+	defer os.Remove(f.Name())
+	assert.True(t, proto.Equal(pb, defaultConfig()))
+}
+
+func TestDefaultConfig(t *testing.T) {
+	path := filepath.Join("testdata", t.Name()+".golden")
+	if *update {
+		ioutil.WriteFile(path, []byte(defaultConfigString()), 0644)
+	}
+
+	golden, _ := ioutil.ReadFile(path)
+	assert.Equal(t, string(golden), defaultConfigString())
+}
