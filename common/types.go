f12403bfcc7836f1d8e4331a2c05d0b7d054d1b1
Saehan Park <saehan@Saehan-MacBook.local>
2018-03-26 12:38:54 +0900

    add byte utility functions and common types including Address and Hash

---
+++ common/types.go
@@ -0,0 +1,70 @@
+package common
+
+import (
+  "bytes"
+  "math/big"
+)
+
+const (
+  HashLength    = 32
+  AddressLength = 20
+)
+
+type Hash [HashLength]byte
+
+func BytesToHash(b []byte) Hash {
+  var h Hash
+  h.SetBytes(b)
+  return h
+}
+func BigToHash(b *big.Int) Hash { return BytesToHash(b.Bytes()) }
+func HexToHash(s string) Hash   { return BytesToHash(FromHex(s)) }
+
+func (h Hash) Str() string   { return string(h[:]) }
+func (h Hash) Bytes() []byte { return h[:] }
+
+func (h *Hash) SetBytes(b []byte) {
+  if len(b) > len(h) {
+    b = b[len(b)-HashLength:]
+  }
+
+  copy(h[HashLength-len(b):], b)
+}
+
+func (a Hash) Equals(b Hash) bool {
+  return bytes.Compare(a[:], b[:]) == 0
+}
+
+type Address [AddressLength]byte
+
+func BytesToAddress(b []byte) Address {
+  var a Address
+  a.SetBytes(b)
+  return a
+}
+func BigToAddress(b *big.Int) Address { return BytesToAddress(b.Bytes()) }
+func HexToAddress(s string) Address   { return BytesToAddress(FromHex(s)) }
+
+func IsHexAddress(s string) bool {
+  if hasHexPrefix(s) {
+    s = s[2:]
+  }
+  return len(s) == 2*AddressLength && isHex(s)
+}
+
+func (a Address) Str() string   { return string(a[:]) }
+func (a Address) Bytes() []byte { return a[:] }
+func (a Address) Big() *big.Int { return new(big.Int).SetBytes(a[:]) }
+func (a Address) Hash() Hash    { return BytesToHash(a[:]) }
+
+// Sets the address to the value of b. If b is larger than len(a) it will panic
+func (a *Address) SetBytes(b []byte) {
+  if len(b) > len(a) {
+    b = b[len(b)-AddressLength:]
+  }
+  copy(a[AddressLength-len(b):], b)
+}
+
+func (a Address) Equals(b Address) bool {
+  return bytes.Compare(a[:], b[:]) == 0
+}
---
+++ common/types_test.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBytesConversion(t *testing.T) {
+	bytes := []byte{5}
+	hash := BytesToHash(bytes)
+
+	var exp Hash
+	exp[31] = 5
+
+	assert.Equalf(t, hash, exp, "expected %x got %x", exp, hash)
+}
+
+func TestIsHexAddress(t *testing.T) {
+	tests := []struct {
+		str string
+		exp bool
+	}{
+		{"0x5aaeb6053f3e94c9b9a09f33669435e7ef1beaed", true},
+		{"5aaeb6053f3e94c9b9a09f33669435e7ef1beaed", true},
+		{"0X5aaeb6053f3e94c9b9a09f33669435e7ef1beaed", true},
+		{"0XAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", true},
+		{"0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", true},
+		{"0x5aaeb6053f3e94c9b9a09f33669435e7ef1beaed1", false},
+		{"0x5aaeb6053f3e94c9b9a09f33669435e7ef1beae", false},
+		{"5aaeb6053f3e94c9b9a09f33669435e7ef1beaed11", false},
+		{"0xxaaeb6053f3e94c9b9a09f33669435e7ef1beaed", false},
+	}
+
+	for _, test := range tests {
+		result := IsHexAddress(test.str)
+		assert.Equalf(t, result, test.exp, "IsHexAddress(%s) == %v; expected %v",
+			test.str, result, test.exp)
+	}
+}
