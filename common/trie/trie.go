2d05e00d29742b9a01d112e4571c9791396e7049
heekyu <heekyu83@gmail.com>
2018-03-30 15:48:37 +0900

    implement trie

---
+++ common/trie/trie.go
@@ -0,0 +1,394 @@
+package trie
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/medibloc/go-medibloc/common/trie/pb"
+	"github.com/medibloc/go-medibloc/storage"
+	"golang.org/x/crypto/sha3"
+)
+
+// Errors
+var (
+	ErrInvalidNodeType = errors.New("invalid node type")
+	ErrPbMsg           = errors.New("Pb Message cannot be converted into Node")
+	ErrNotFound        = storage.ErrKeyNotFound
+)
+
+// Flag to identify the type of node
+type ty uint8
+
+// const Node types
+const (
+	unknown ty = iota
+	branch
+	ext
+	leaf
+)
+
+// Node in trie, three kinds,
+// Branch Node [hash_0, hash_1, ..., hash_f]
+// Extension Node [flag, encodedPath, next hash]
+// Leaf Node [flag, encodedPath, value]
+type node struct {
+	Type ty
+	Val  [][]byte
+}
+
+func (n *node) toProto() (proto.Message, error) {
+	return &triepb.Node{
+		Type: uint32(n.Type),
+		Val:  n.Val,
+	}, nil
+}
+
+func (n *node) fromProto(msg proto.Message) error {
+	if msg, ok := msg.(*triepb.Node); ok {
+		n.Type = ty(msg.Type)
+		n.Val = msg.Val
+		return nil
+	}
+	return ErrPbMsg
+}
+
+// Trie is a Merkle Patricia Trie, consists of three kinds of nodes,
+// Branch Node: 16-elements array, value is [hash_0, hash_1, ..., hash_f, hash]
+// Extension Node: 3-elements array, value is [ext flag, prefi path, next hash]
+// Leaf Node: 3-elements array, value is [leaf flag, suffix path, value]
+type Trie struct {
+	rootHash []byte
+	storage  storage.Storage
+}
+
+func NewTrie(rootHash []byte, storage storage.Storage) (*Trie, error) {
+	switch {
+	case rootHash != nil:
+		if _, err := storage.Get(rootHash); err != nil {
+			return nil, err
+		}
+	}
+	return &Trie{rootHash: rootHash, storage: storage}, nil
+}
+
+// Delete delete value from Trie
+func (t *Trie) Delete(key []byte) error {
+	hash, err := t.delete(t.rootHash, keyToRoute(key))
+	if err != nil {
+		return err
+	}
+	t.rootHash = hash
+	return nil
+}
+
+// Get get value from Trie
+func (t *Trie) Get(key []byte) ([]byte, error) {
+	return t.get(t.rootHash, keyToRoute(key))
+}
+
+// Put put value to Trie
+func (t *Trie) Put(key []byte, value []byte) error {
+	route := keyToRoute(key)
+	var hash []byte
+	var err error
+	if t.rootHash == nil {
+		leaf := newLeafNode(route, value)
+		if hash, err = t.saveNode(leaf); err != nil {
+			return err
+		}
+	} else if hash, err = t.update(t.rootHash, route, value); err != nil {
+		return err
+	}
+
+	t.rootHash = hash
+	return nil
+}
+
+func (t *Trie) RootHash() []byte {
+	return t.rootHash
+}
+
+func (t *Trie) SetRootHash(rootHash []byte) {
+	t.rootHash = rootHash
+}
+
+func (t *Trie) addExtToBranch(branchHash []byte, path []byte) ([]byte, error) {
+	var hash []byte
+	var err error
+	if len(path) > 0 {
+		ext := newExtNode(path, branchHash)
+		if hash, err = t.saveNode(ext); err != nil {
+			return nil, err
+		}
+		return hash, nil
+	}
+	return hash, nil
+}
+
+func (t *Trie) delete(rootHash []byte, route []byte) ([]byte, error) {
+	n, err := t.fetchNode(rootHash)
+	if err != nil {
+		return nil, err
+	}
+	var hash []byte
+	switch n.Type {
+	case branch:
+		if hash, err = t.delete(n.Val[route[0]], route[1:]); err != nil {
+			return nil, err
+		}
+		// TODO make it ext or leaf if can do
+		n.Val[route[0]] = hash
+		if hash == nil && isBranchEmpty(n) {
+			return nil, nil
+		}
+		return t.saveNode(n)
+	case ext:
+		fmt.Println("DEL ext")
+		path := n.Val[0]
+		next := n.Val[1]
+		matchLen := prefixLen(path, route)
+		if matchLen != len(path) {
+			return nil, ErrNotFound
+		}
+		if hash, err = t.delete(next, route[matchLen:]); err != nil {
+			return nil, err
+		}
+		if hash == nil {
+			return nil, nil
+		}
+		n.Val[1] = hash
+		return t.saveNode(n)
+	case leaf:
+		return nil, nil
+	default:
+		return nil, ErrInvalidNodeType
+	}
+}
+
+func (t *Trie) fetchNode(hash []byte) (*node, error) {
+	value, err := t.storage.Get(hash)
+	if err != nil {
+		return nil, err
+	}
+	pb := new(triepb.Node)
+	if err := proto.Unmarshal(value, pb); err != nil {
+		return nil, err
+	}
+	n := new(node)
+	if err := n.fromProto(pb); err != nil {
+		return nil, err
+	}
+	if n.Type < branch || n.Type > leaf {
+		return nil, ErrInvalidNodeType
+	}
+	return n, nil
+}
+
+func (t *Trie) get(root []byte, route []byte) ([]byte, error) {
+	fmt.Println("GET START")
+	curRootHash := root
+	curRoute := route
+	for len(curRoute) >= 0 {
+		rootNode, err := t.fetchNode(curRootHash)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("current type is ", rootNode.Type)
+		switch rootNode.Type {
+		case branch:
+			curRootHash = rootNode.Val[curRoute[0]]
+			curRoute = curRoute[1:]
+		case ext:
+			path := rootNode.Val[0]
+			next := rootNode.Val[1]
+			matchLen := prefixLen(path, curRoute)
+			if matchLen != len(path) {
+				return nil, ErrNotFound
+			}
+			curRootHash = next
+			curRoute = curRoute[matchLen:]
+		case leaf:
+			path := rootNode.Val[0]
+			matchLen := prefixLen(path, curRoute)
+			if matchLen != len(path) || matchLen != len(curRoute) {
+				return nil, ErrNotFound
+			}
+			return rootNode.Val[1], nil
+		default:
+			return nil, ErrInvalidNodeType
+		}
+	}
+	return nil, ErrNotFound
+}
+
+func (t *Trie) saveNode(n *node) ([]byte, error) {
+	pb, err := n.toProto()
+	if err != nil {
+		return nil, err
+	}
+	b, err := proto.Marshal(pb)
+	if err != nil {
+		return nil, err
+	}
+	hash256 := sha3.Sum256(b)
+	hash := hash256[:]
+	t.storage.Put(hash, b)
+	return hash, nil
+}
+
+func (t *Trie) update(root []byte, route []byte, value []byte) ([]byte, error) {
+	n, err := t.fetchNode(root)
+	if err != nil {
+		return nil, err
+	}
+	switch n.Type {
+	case branch:
+		return t.updateBranch(n, route, value)
+	case ext:
+		return t.updateExt(n, route, value)
+	case leaf:
+		return t.updateLeaf(n, route, value)
+	default:
+		return nil, ErrInvalidNodeType
+	}
+}
+
+func (t *Trie) updateBranch(n *node, route []byte, value []byte) ([]byte, error) {
+	newHash, err := t.update(n.Val[route[0]], route[1:], value)
+	if err != nil {
+		return nil, err
+	}
+	n.Val[route[0]] = newHash
+	var hash []byte
+	if hash, err = t.saveNode(n); err != nil {
+		return nil, err
+	}
+	return hash, nil
+}
+
+func (t *Trie) updateExt(n *node, route []byte, value []byte) ([]byte, error) {
+	path := n.Val[0]
+	next := n.Val[1]
+	matchLen := prefixLen(path, route)
+	var hash []byte
+	var err error
+	if matchLen == len(path) {
+		if hash, err = t.update(next, route[matchLen:], value); err != nil {
+			return nil, err
+		}
+		n.Val[1] = hash
+		return t.saveNode(n)
+	}
+	branch := emptyBranchNode()
+	if len(path) > 1 {
+		n.Val[0] = path[matchLen+1:]
+		if hash, err = t.saveNode(n); err != nil {
+			return nil, err
+		}
+		branch.Val[path[matchLen]] = hash
+	} else {
+		branch.Val[path[0]] = next
+	}
+
+	leaf := newLeafNode(route[matchLen+1:], value)
+	if hash, err = t.saveNode(leaf); err != nil {
+		return nil, err
+	}
+	branch.Val[route[matchLen]] = hash
+
+	if hash, err = t.saveNode(branch); err != nil {
+		return nil, err
+	}
+	return t.addExtToBranch(hash, path[:matchLen])
+}
+
+func (t *Trie) updateLeaf(n *node, route []byte, value []byte) ([]byte, error) {
+	path := n.Val[0]
+	if len(path) > len(route) {
+		return nil, errors.New("short key")
+	}
+	// TODO heekyu when len(path) < len(route) ? is this impossible?
+	matchLen := prefixLen(path, route)
+	if matchLen == len(route) {
+		n.Val[1] = value
+		hash, err := t.saveNode(n)
+		if err != nil {
+			return nil, err
+		}
+		return hash, nil
+	}
+	var hash []byte
+	var err error
+	branch := emptyBranchNode()
+	n.Val[0] = path[matchLen+1:]
+	if hash, err = t.saveNode(n); err != nil {
+		return nil, err
+	}
+	branch.Val[path[matchLen]] = hash
+
+	leaf := newLeafNode(route[matchLen+1:], value)
+	if hash, err = t.saveNode(leaf); err != nil {
+		return nil, err
+	}
+	branch.Val[route[matchLen]] = hash
+
+	if hash, err = t.saveNode(branch); err != nil {
+		return nil, err
+	}
+	return t.addExtToBranch(hash, path[:matchLen])
+}
+
+func emptyBranchNode() *node {
+	return &node{
+		Type: branch,
+		Val:  [][]byte{nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil},
+	}
+}
+
+// keyToRoute 256 => 16 * 16
+func keyToRoute(key []byte) []byte {
+	l := len(key) * 2
+	var route = make([]byte, l)
+	for i, b := range key {
+		route[i*2] = b / 16
+		route[i*2+1] = b % 16
+	}
+	return route
+}
+
+func isBranchEmpty(n *node) bool {
+	for _, v := range n.Val {
+		if v != nil && len(v) > 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func newExtNode(path []byte, next []byte) *node {
+	return &node{
+		Type: ext,
+		Val:  [][]byte{path, next},
+	}
+}
+
+func newLeafNode(path []byte, value []byte) *node {
+	return &node{
+		Type: leaf,
+		Val:  [][]byte{path, value},
+	}
+}
+
+func prefixLen(a, b []byte) int {
+	length := len(a)
+	if len(b) < length {
+		length = len(b)
+	}
+	for i := 0; i < length; i += 1 {
+		if a[i] != b[i] {
+			return i
+		}
+	}
+	return length
+}
---
+++ common/trie/trie_test.go
@@ -0,0 +1,119 @@
+// Copyright 2018 The go-medibloc Authors
+// This file is part of the go-medibloc library.
+//
+// The go-medibloc library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-medibloc library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-medibloc library. If not, see <http://www.gnu.org/licenses/>.
+
+package trie_test
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/medibloc/go-medibloc/common/trie"
+	"github.com/medibloc/go-medibloc/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func getItems() {
+	var items = make(map[[5]byte][]byte)
+
+	items[[5]byte{11, 59, 53, 34, 5}] = []byte{1, 1, 1, 1, 1, 1, 1}
+	items[[5]byte{11, 49, 63, 44, 5}] = []byte{1, 1, 1, 1, 1, 1, 1}
+	items[[5]byte{21, 39, 73, 54, 5}] = []byte{1, 1, 1, 1, 1, 1, 1}
+	items[[5]byte{31, 29, 83, 64, 5}] = []byte{1, 1, 1, 1, 1, 1, 1}
+	items[[5]byte{41, 19, 93, 74, 5}] = []byte{1, 1, 1, 1, 1, 1, 1}
+}
+
+func TestNewTrie(t *testing.T) {
+	type args struct {
+		rootHash []byte
+	}
+	s, err := storage.NewMemoryStorage()
+	assert.Nil(t, err)
+	tests := []struct {
+		args    args
+		name    string
+		storage storage.Storage
+		wantErr bool
+	}{
+		{
+			args{nil},
+			"test1",
+			s,
+			false,
+		},
+		{
+			args{[]byte("root")},
+			"test2",
+			s,
+			true,
+		},
+	}
+
+	for _, t2 := range tests {
+		t.Run(t2.name, func(t3 *testing.T) {
+			tr, err := trie.NewTrie(t2.args.rootHash, t2.storage)
+			assert.Equal(t3, t2.wantErr, err != nil)
+			if err == nil {
+				assert.Equal(t3, tr.RootHash(), t2.args.rootHash)
+			}
+		})
+	}
+}
+
+func TestTrie_Operations(t *testing.T) {
+	s, err := storage.NewMemoryStorage()
+	assert.Nil(t, err)
+
+	tr, err := trie.NewTrie(nil, s)
+	assert.Nil(t, err)
+	assert.NotNil(t, tr)
+
+	key1, _ := hex.DecodeString("2ed1b10c")
+	val1 := []byte("leafmedi1")
+	err = tr.Put(key1, val1)
+	assert.Nil(t, err)
+	curRootHash := []byte{0xe5, 0xa2, 0x8b, 0x11, 0x2e, 0x6f, 0x65, 0x91, 0xbd, 0xa4, 0x37, 0xd3, 0x86, 0xa9, 0x19, 0xd9, 0xb3, 0x5d, 0x9d, 0x86, 0x3d, 0x6e, 0xd6, 0x5b, 0x19, 0x3, 0x6c, 0x7, 0x6f, 0x25, 0xd4, 0x85}
+	assert.Equal(t, curRootHash, tr.RootHash())
+
+	val2, err := tr.Get(key1)
+	assert.Nil(t, err)
+	assert.Equal(t, val2, val1)
+
+	err = tr.Delete(key1)
+	assert.Nil(t, err)
+	assert.Nil(t, tr.RootHash())
+
+	err = tr.Put(key1, val1)
+	assert.Nil(t, err)
+
+	key2, _ := hex.DecodeString("2ed1c000")
+	val2 = []byte("leafmedi2")
+	err = tr.Put(key2, val2)
+	assert.Nil(t, err)
+
+	err = tr.Delete(key2)
+	assert.Nil(t, err)
+
+	val2, err = tr.Get(key1)
+	assert.Nil(t, err)
+	assert.Equal(t, val2, val1)
+	// TODO make it same topology after put & delete
+	// assert.Equal(t, curRootHash, tr.RootHash())
+
+	err = tr.Delete(key1)
+	assert.Nil(t, err)
+	tr.Get(key2)
+	assert.Nil(t, tr.RootHash())
+}
