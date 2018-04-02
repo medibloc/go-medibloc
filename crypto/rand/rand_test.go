500fee793fd63ab6741f1f420b2c0aba4e0b375a
Saehan Park <saehan@Saehan-MacBook.local>
2018-04-02 11:21:52 +0900

    fix rand test code error

--- crypto/rand/rand_test.go
+++ crypto/rand/rand_test.go
@@ -3,12 +3,11 @@ package rand
 import (
 	"testing"
 
-	"github.com/medibloc/go-medibloc/crypto/rand"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRandom(t *testing.T) {
-	a := rand.GetEntropyCSPRNG(32)
-	b := rand.GetEntropyCSPRNG(32)
+	a := GetEntropyCSPRNG(32)
+	b := GetEntropyCSPRNG(32)
 	assert.NotEqual(t, a, b, "Two random outputs should not equal")
 }
