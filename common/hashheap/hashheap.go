b58fb0ca198533f37682ee2bbc7d96977164cdbc
Cha Minkyoo <minkyoo.cha@medibloc.org>
2018-04-11 11:43:38 +0900

    Fix lint of hashheap

--- common/hashheap/hashheap.go
+++ common/hashheap/hashheap.go
@@ -2,15 +2,18 @@ package hashheap
 
 import "container/heap"
 
+// Comparable is an interface that allows object to order through comparison.
 type Comparable interface {
 	Less(b interface{}) bool
 }
 
+// HashedHeap is a data structure for hash and priority queue.
 type HashedHeap struct {
 	pq   priorityQueue
 	hash map[string]*entry
 }
 
+// NewHashedHeap returns HashedHeap.
 func NewHashedHeap() *HashedHeap {
 	return &HashedHeap{
 		pq:   make(priorityQueue, 0),
@@ -18,6 +21,7 @@ func NewHashedHeap() *HashedHeap {
 	}
 }
 
+// Get get value by key.
 func (h *HashedHeap) Get(key string) (value Comparable) {
 	e, ok := h.hash[key]
 	if !ok {
@@ -26,6 +30,7 @@ func (h *HashedHeap) Get(key string) (value Comparable) {
 	return e.value
 }
 
+// Set set value by key.
 func (h *HashedHeap) Set(key string, value Comparable) {
 	if _, ok := h.hash[key]; ok {
 		h.Del(key)
@@ -39,6 +44,7 @@ func (h *HashedHeap) Set(key string, value Comparable) {
 	heap.Push(&h.pq, e)
 }
 
+// Del deletes value by key.
 func (h *HashedHeap) Del(key string) {
 	e, ok := h.hash[key]
 	if !ok {
@@ -48,6 +54,7 @@ func (h *HashedHeap) Del(key string) {
 	delete(h.hash, key)
 }
 
+// Pop pop value by priority.
 func (h *HashedHeap) Pop() (value Comparable) {
 	if h.pq.Len() == 0 {
 		return nil
@@ -57,6 +64,7 @@ func (h *HashedHeap) Pop() (value Comparable) {
 	return e.value
 }
 
+// Peek peek value by priority.
 func (h *HashedHeap) Peek() (value Comparable) {
 	if h.pq.Len() == 0 {
 		return nil
