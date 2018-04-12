7cfbef7c78c1a00dd3bb62834b311fa8b826d41d
ggomma <ggomma@gmail.com>
2018-04-12 16:38:06 +0900

    Add make.sh file
     1. Run $ sh make.sh
     2. It will automatically generate *pb.go and *pb.gw.go file

---
+++ rpc/proto/make.sh
@@ -0,0 +1,6 @@
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:. \
+  --go_out=plugins=grpc:. \
+  ./rpc.proto
