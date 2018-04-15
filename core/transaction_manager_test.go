7373d3430a210f74cd68b011e632fc6471ddc96a
Cha Minkyoo <minkyoo.cha@medibloc.org>
2018-04-15 23:45:22 +0900

    Add TransactionManager's test cases

--- core/transaction_manager_test.go
+++ core/transaction_manager_test.go
@@ -32,3 +32,80 @@ func TestTransactionManager(t *testing.T) {
 	}
 	assert.EqualValues(t, tx.Hash(), actual.Hash())
 }
+
+func TestTransactionManagerAbnormalTx(t *testing.T) {
+	mgrs, closeFn := newTestTransactionManagers(t, 2)
+	defer closeFn()
+
+	sender, receiver := mgrs[0], mgrs[1]
+
+	// No signature
+	noSign := newRandomTransaction(t)
+	expectTxFiltered(t, sender, receiver, noSign)
+
+	// Invalid signature
+	from, to := newPrivateKey(t), newPrivateKey(t)
+	invalidSign := newTransaction(t, from, to, 10)
+	signTx(t, invalidSign, to)
+	expectTxFiltered(t, sender, receiver, invalidSign)
+}
+
+func TestTransactionManagerDupTxFromNet(t *testing.T) {
+	mgrs, closeFn := newTestTransactionManagers(t, 2)
+	defer closeFn()
+
+	sender, receiver := mgrs[0], mgrs[1]
+
+	dup := newRandomSignedTransaction(t)
+	sender.Broadcast(dup)
+	sender.Broadcast(dup)
+	time.Sleep(100 * time.Millisecond)
+
+	normal := newRandomSignedTransaction(t)
+	sender.Broadcast(normal)
+
+	var count int
+	for {
+		recv := receiver.Pop()
+		if recv != nil && recv.Hash() == normal.Hash() {
+			break
+		}
+		if recv != nil {
+			count++
+		}
+		time.Sleep(time.Millisecond)
+	}
+	assert.Equal(t, 1, count)
+}
+
+func TestTranactionManagerDupTxPush(t *testing.T) {
+	mgrs, closeFn := newTestTransactionManagers(t, 2)
+	defer closeFn()
+
+	dup := newRandomSignedTransaction(t)
+	err := mgrs[0].Push(dup)
+	assert.NoError(t, err)
+	err = mgrs[0].Push(dup)
+	assert.EqualValues(t, core.ErrDuplicatedTransaction, err)
+
+	actual := mgrs[0].Pop()
+	assert.EqualValues(t, dup, actual)
+	actual = mgrs[0].Pop()
+	assert.Nil(t, actual)
+}
+
+func expectTxFiltered(t *testing.T, sender, receiver *core.TransactionManager, abnormal *core.Transaction) {
+	sender.Broadcast(abnormal)
+
+	time.Sleep(100 * time.Millisecond)
+
+	normal := newRandomSignedTransaction(t)
+	sender.Broadcast(normal)
+
+	var recv *core.Transaction
+	for recv == nil {
+		recv = receiver.Pop()
+		time.Sleep(time.Millisecond)
+	}
+	assert.EqualValues(t, normal.Hash(), recv.Hash())
+}
