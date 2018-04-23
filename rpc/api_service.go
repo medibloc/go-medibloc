48b96d979d3ea5d90a2d73313ae37911ce829840
heekyu <heekyu83@gmail.com>
2018-04-23 16:17:31 +0900

    remove fake data codes

--- rpc/api_service.go
+++ rpc/api_service.go
@@ -6,55 +6,6 @@ import (
 	"golang.org/x/net/context"
 )
 
-//
-// FAKE DATA BEGIN
-//
-
-type account struct {
-	address string
-	balance float32
-	nonce   uint64
-	ttype   uint32
-}
-
-func makeFakeBlockChain() map[uint64][]*account {
-	blockChain := map[uint64][]*account{
-		1: []*account{
-			&account{
-				address: "M00001",
-				balance: 1,
-				nonce:   2,
-				ttype:   1,
-			},
-			&account{
-				address: "M00002",
-				balance: 1,
-				nonce:   2,
-				ttype:   1,
-			},
-		},
-		2: []*account{
-			&account{
-				address: "M00001",
-				balance: 5,
-				nonce:   6,
-				ttype:   1,
-			},
-			&account{
-				address: "M00002",
-				balance: 7,
-				nonce:   12,
-				ttype:   1,
-			},
-		},
-	}
-	return blockChain
-}
-
-//
-// FAKE DATA ENDS
-//
-
 // APIService is blockchain api rpc service.
 type APIService struct {
 	bridge Bridge
@@ -82,12 +33,12 @@ func (s *APIService) GetMedState(ctx context.Context, req *rpcpb.NonParamsReques
 // GetAccountState handles GetAccountState rpc.
 func (s *APIService) GetAccountState(ctx context.Context, req *rpcpb.GetAccountStateRequest) (*rpcpb.GetAccountStateResponse, error) {
 	bm := s.bridge.BlockManager()
+	// height := req.Height TODO get state for height
 	tailBlock := bm.TailBlock()
 	acc, err := tailBlock.State().GetAccount(common.HexToAddress(req.Address))
 	if err != nil {
 		return nil, err
 	}
-	// height := req.Height TODO get state for height
 	return &rpcpb.GetAccountStateResponse{
 		Balance: acc.Balance().String(),
 		Nonce:   acc.Nonce(),
