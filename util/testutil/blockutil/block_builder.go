ff84571c231b6c7468918cc2b065828064af0315
Seunghwan Kim <shwankim@medibloc.org>
2018-07-17 19:34:30 +0900

    Fix excute function on blockbuilder

--- util/testutil/blockutil/block_builder.go
+++ util/testutil/blockutil/block_builder.go
@@ -262,7 +262,8 @@ func (bb *BlockBuilder) ExecuteTx(tx *core.Transaction) *BlockBuilder {
 	n := bb.copy()
 
 	require.NoError(n.t, n.B.BeginBatch())
-	require.NoError(n.t, n.B.Execute(tx, defaultTxMap))
+	require.NoError(n.t, n.B.ExecuteTransaction(tx, defaultTxMap))
+	require.NoError(n.t, n.B.AcceptTransaction(tx))
 	require.NoError(bb.t, n.B.Commit())
 
 	return n
@@ -272,7 +273,7 @@ func (bb *BlockBuilder) ExecuteTxErr(tx *core.Transaction, expected error) *Bloc
 	n := bb.copy()
 
 	require.NoError(n.t, n.B.BeginBatch())
-	err := n.B.Execute(tx, defaultTxMap)
+	err := n.B.ExecuteTransaction(tx, defaultTxMap)
 	if err != nil {
 		require.Equal(n.t, expected, err)
 		return n
