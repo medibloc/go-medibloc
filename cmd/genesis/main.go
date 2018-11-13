ad00b22178be3af2d23fbb978b833bce9cc95a7c
Seunghwan Kim <shwankim@medibloc.org>
2018-11-13 10:58:37 +0900

    change genesis conf for testnet

--- cmd/genesis/main.go
+++ cmd/genesis/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gogo/protobuf/proto"
 	"github.com/medibloc/go-medibloc/consensus/dpos"
 	"github.com/medibloc/go-medibloc/core"
 	"github.com/medibloc/go-medibloc/core/pb"
 	"github.com/medibloc/go-medibloc/medlet"
 	"github.com/medibloc/go-medibloc/storage"
+	"github.com/medibloc/go-medibloc/util/byteutils"
+	"io/ioutil"
 )
 
 func main() {
+	bytes, err := ioutil.ReadFile("conf/testnet/genesis.conf")
+	if err != nil {
+		fmt.Println("failed to load genesis conf file")
+		return
+	}
+	pbGenesis:= new(corepb.Genesis)
+	if err := proto.UnmarshalText(string(bytes), pbGenesis); err != nil {
+		fmt.Println("failed unmarshal to corepb.Genesis")
+		return
+	}
 
-	//aa := corepb.Genesis{
-	//	Meta:              nil,
-	//	TokenDistribution: nil,
-	//	Transactions:      nil,
-	//}
+	hash, err := CalcGenesisHashFromGenesisConfig(pbGenesis)
+	if err != nil {
+		fmt.Println("failed to calculate genesis hash")
+		return
+	}
+
+	fmt.Println("genesis hash:", byteutils.Bytes2Hex(hash))
+	return
 
 }
 
