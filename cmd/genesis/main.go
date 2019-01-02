510bfe2842ff859ae06e35197273f1092204989f
Seunghwan Kim <shwankim@medibloc.org>
2019-01-02 21:27:56 +0900

    Implement genesis config generator for testnet

--- cmd/genesis/main.go
+++ cmd/genesis/main.go
@@ -3,47 +3,166 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 
 	"github.com/gogo/protobuf/proto"
+	"github.com/medibloc/go-medibloc/common"
 	"github.com/medibloc/go-medibloc/consensus/dpos"
 	"github.com/medibloc/go-medibloc/core"
-	corepb "github.com/medibloc/go-medibloc/core/pb"
-	"github.com/medibloc/go-medibloc/medlet"
-	"github.com/medibloc/go-medibloc/storage"
+	"github.com/medibloc/go-medibloc/core/pb"
+	"github.com/medibloc/go-medibloc/crypto"
+	"github.com/medibloc/go-medibloc/crypto/signature/algorithm"
+	"github.com/medibloc/go-medibloc/crypto/signature/secp256k1"
+	"github.com/medibloc/go-medibloc/medlet/pb"
+	"github.com/medibloc/go-medibloc/util"
 	"github.com/medibloc/go-medibloc/util/byteutils"
 )
 
-func main() {
-	bytes, err := ioutil.ReadFile("conf/testnet/genesis.conf")
-	if err != nil {
-		fmt.Println("failed to load genesis conf file")
-		return
+func randomPrivateKeys() []string {
+	privKeys := make([]string, 25)
+	for i := 0; i < 25; i++ {
+		key, err := crypto.GenerateKey(algorithm.SECP256K1)
+		if err != nil {
+			panic("failed to generate key")
+		}
+		keyBytes, err := key.Encoded()
+		if err != nil {
+			panic("failed to generate key")
+		}
+		privKeys[i] = byteutils.Bytes2Hex(keyBytes)
 	}
-	pbGenesis := new(corepb.Genesis)
-	if err := proto.UnmarshalText(string(bytes), pbGenesis); err != nil {
-		fmt.Println("failed unmarshal to corepb.Genesis")
-		return
-	}
-
-	hash, err := CalcGenesisHashFromGenesisConfig(pbGenesis)
-	if err != nil {
-		fmt.Println("failed to calculate genesis hash")
-		return
-	}
-
-	fmt.Println("genesis hash:", byteutils.Bytes2Hex(hash))
-	return
-
+	return privKeys
 }
 
-// CalcGenesisHashFromGenesisConfig returns hash of genesis block
-func CalcGenesisHashFromGenesisConfig(genesis *corepb.Genesis) ([]byte, error) {
-	consenesus := dpos.New(int(genesis.Meta.DynastySize))
-	stor, err := storage.NewMemoryStorage()
-	if err != nil {
-		return nil, err
+func main() {
+	//privKeys := testnet_privKeys()
+	privKeys := randomPrivateKeys()
+
+	const chainId = 181228
+	const dynastySize = 21
+
+	chainCfg := new(medletpb.ChainConfig)
+	chainCfg.Proposers = make([]*medletpb.ProposerConfig, dynastySize)
+
+	conf := &corepb.Genesis{
+		Meta: &corepb.GenesisMeta{
+			ChainId:     chainId,
+			DynastySize: uint32(dynastySize),
+		},
+		TokenDistribution: nil,
+		Transactions:      nil,
 	}
 
-	b, err := core.NewGenesisBlock(genesis, consenesus, medlet.DefaultTxMap, stor)
-	return b.Hash(), nil
+	var tokenDist []*corepb.GenesisTokenDistribution
+	txs := make([]*core.Transaction, 0)
+
+	for i, v := range privKeys[:dynastySize] {
+		key, err := secp256k1.NewPrivateKeyFromHex(v)
+		if err != nil {
+			fmt.Println("failed to convert string to private key")
+			return
+		}
+		sig, _ := crypto.NewSignature(algorithm.SECP256K1)
+		sig.InitSign(key)
+
+		addr, err := common.PublicKeyToAddress(key.PublicKey())
+
+		proposer := &medletpb.ProposerConfig{
+			Proposer: addr.Hex(),
+			Privkey:  v,
+			Coinbase: addr.Hex(),
+		}
+
+		chainCfg.Proposers[i] = proposer
+
+		tokenDist = append(tokenDist, &corepb.GenesisTokenDistribution{
+			Address: addr.Hex(),
+			Balance: "320000000000000000000",
+		})
+
+		vesting, _ := util.NewUint128FromString("100000000000000000000")
+		collateral, _ := util.NewUint128FromString(dpos.MinimumCandidateCollateral)
+		aliasCollateral, _ := util.NewUint128FromString(core.MinimumAliasCollateral)
+
+		tx := new(core.Transaction)
+
+		tx.SetChainID(chainId)
+		tx.SetValue(util.NewUint128())
+
+		txVest, _ := tx.Clone()
+		txVest.SetTxType(core.TxOpStake)
+		txVest.SetValue(vesting)
+		txVest.SetNonce(1)
+		txVest.SignThis(key)
+
+		aliasPayload := &core.RegisterAliasPayload{AliasName: "testbp" + strconv.Itoa(i)}
+		aliasePayloadBytes, err := aliasPayload.ToBytes()
+
+		txAlias, err := tx.Clone()
+		txAlias.SetTxType(core.TxOpRegisterAlias)
+		txAlias.SetValue(aliasCollateral)
+		txAlias.SetNonce(2)
+		txAlias.SetPayload(aliasePayloadBytes)
+		txAlias.SignThis(key)
+
+		becomeCandidatePayload := &dpos.BecomeCandidatePayload{URL: "www.medibloc.org"}
+		becomeCandidatePayloadBytes, _ := becomeCandidatePayload.ToBytes()
+
+		txCandidate, _ := tx.Clone()
+		txCandidate.SetTxType(dpos.TxOpBecomeCandidate)
+		txCandidate.SetValue(collateral)
+		txCandidate.SetNonce(3)
+		txCandidate.SetPayload(becomeCandidatePayloadBytes)
+		txCandidate.SignThis(key)
+
+		votePayload := new(dpos.VotePayload)
+		candidateIds := make([][]byte, 0)
+		votePayload.CandidateIDs = append(candidateIds, txCandidate.Hash())
+		votePayloadBytes, _ := votePayload.ToBytes()
+
+		txVote, _ := tx.Clone()
+		txVote.SetTxType(dpos.TxOpVote)
+		txVote.SetNonce(4)
+		txVote.SetPayload(votePayloadBytes)
+		txVote.SignThis(key)
+
+		txs = append(txs, txVest, txAlias, txCandidate, txVote)
+	}
+
+	for _, v := range privKeys[dynastySize:] {
+		key, err := secp256k1.NewPrivateKeyFromHex(v)
+		if err != nil {
+			fmt.Println("failed to convert string to private key")
+			return
+		}
+		sig, _ := crypto.NewSignature(algorithm.SECP256K1)
+		sig.InitSign(key)
+
+		addr, err := common.PublicKeyToAddress(key.PublicKey())
+
+		tokenDist = append(tokenDist, &corepb.GenesisTokenDistribution{
+			Address: addr.Hex(),
+			Balance: "320000000000000000000",
+		})
+	}
+
+	conf.TokenDistribution = tokenDist
+	conf.Transactions = make([]*corepb.Transaction, 0)
+	for _, v := range txs {
+		pbMsg, _ := v.ToProto()
+		pbTx, _ := pbMsg.(*corepb.Transaction)
+		conf.Transactions = append(conf.Transactions, pbTx)
+	}
+
+	chainCfgTxt := proto.MarshalTextString(chainCfg)
+	err := ioutil.WriteFile("./cmd/genesis/node.conf", []byte(chainCfgTxt), 400)
+	if err != nil {
+		panic("failed to save file chain config")
+	}
+
+	genesisTxt := proto.MarshalTextString(conf)
+	err = ioutil.WriteFile("./cmd/genesis/genesis.conf", []byte(genesisTxt), 400)
+	if err != nil {
+		panic("failed to save file genesis config")
+	}
 }
