e03c8baf2a4dc1c7994bba91b28e162ea1f14205
Cha Minkyoo <minkyoo.cha@medibloc.org>
2019-01-30 17:57:26 +0900

    Inject tx mapper dependency for tests

--- util/testutil/blockutil/types.go
+++ util/testutil/blockutil/types.go
@@ -1,5 +1,14 @@
 package blockutil
 
+import (
+	"github.com/medibloc/go-medibloc/core"
+	"github.com/medibloc/go-medibloc/core/transaction"
+)
+
+func init() {
+	core.InjectTxMapper(transaction.DefaultTxMap)
+}
+
 var (
 	// ChainID is chain id for test configuration.
 	ChainID uint32 = 1
